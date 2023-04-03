780b4288c2999d955f1ba4172c3bab1810c1d97d
Horacio <tguni-97@hotmail.com>
2023-04-03 12:11:45 -0300

    run_pinn corregido

--- eq_no_phys.py
+++ eq_no_phys.py
@@ -3,7 +3,7 @@ tf.keras.backend.set_floatx('float32')
 import numpy as np
 
 @tf.function
-def opinion_model(model, coords, params):
+def test(model, coords, params):
     """ Opinion model
     Assumes coords is ordered in space and at constant t
     """
@@ -18,17 +18,8 @@ def opinion_model(model, coords, params):
         tape.watch(coords)
         Yp = model(coords)[0]
         u   = Yp[:,0] 
-        uf  = u*(2*fx - 1)
-
-    #LHS
-    grad_u  = tape.gradient(u, coords)
-    u_t = grad_u[:,0]
-
-    #RHS
-    grad_uf = tape.gradient(uf, coords)
-    uf_x = grad_uf[:,1]
-
+        
     del tape
 
-    f = u_t - uf_x
+    f = u
     return [f]
